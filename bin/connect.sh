b5f1e48bf54766def7c0b25f38a4cf62a7627471
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-01-27 13:54:44 -0500

    copied from my own setup

---
+++ bin/connect.sh
@@ -0,0 +1,7 @@
+docker exec -it oracle-23c sqlplus sys/foo@FREE as sysdba
+docker exec -it oracle-23c sqlplus system/foo@FREE
+docker exec -it oracle-23c sqlplus pdbadmin/foo@FREEPDB1
+
+docker exec -it oracle-23c bash
+sqlplus eoda/foo@FREEPDB1
+sqlplus scott/tiger@localhost:1521/FREEPDB1
