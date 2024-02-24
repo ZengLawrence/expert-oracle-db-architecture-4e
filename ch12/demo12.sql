dc8cbfd16da41bedc09a3fbb285b077634c22585
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-02-24 10:51:19 -0500

    add tkprof commands

--- ch12/demo12.sql
+++ ch12/demo12.sql
@@ -21,6 +21,12 @@ select sum(ln(float_type)) from t;
 select sum(ln(double_type)) from t;
 
 set numformat 999999.9999999999999999
+
+exec dbms_monitor.session_trace_enable;
+
 select sum(ln(num_type)) from t;
 select sum(ln(double_type)) from t;
 select sum(ln(cast( num_type as binary_double ) )) from t;
+
+exec dbms_monitor.session_trace_disable;
+@tk "sys=no"
