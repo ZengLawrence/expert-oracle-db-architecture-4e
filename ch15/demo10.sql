54ef77fd2527e77988c01fa6414a10263d9f8385
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-03-10 11:10:43 -0400

    use a small table to save time

--- ch15/demo10.sql
+++ ch15/demo10.sql
@@ -3,8 +3,8 @@
 select systimestamp from dual;
 set verify off feedback off termout off echo off trimspool on pagesize 0 head off
 set markup csv on
-spo /tmp/big_table.dat
-select * from big_table;
+spo /tmp/dept.dat
+select * from dept;
 spo off;
 set verify on feedback on termout on echo on
 select systimestamp from dual;
